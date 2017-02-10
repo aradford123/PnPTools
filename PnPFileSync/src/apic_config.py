f705a07ac976ddba57af04568a1cc6cb77e93943
Adam Radford (aradford) <aradford@cisco.com>
2017-02-11 09:08:21 +1100

    clarified running the scripts

--- PnPFileSync/src/apic_config.py
+++ PnPFileSync/src/apic_config.py
@@ -2,4 +2,4 @@ import os
 APIC= os.getenv("APIC") or "adam-iwan"
 APIC_USER= os.getenv("APIC_USER") or "admin"
 APIC_PASSWORD= os.getenv("APIC_PASSWORD") or "password"
-DIR="../files"
+DIR=os.getenv('PNP_DIR') or "../files"
