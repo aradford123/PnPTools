bb296fadff3d3c80384494a6aa8f6e6a7f67348e
Adam Radford (aradford) <aradford@cisco.com>
2017-02-18 11:01:29 +1100

    clarified running the scripts

--- PnPFileSync/src/pnp_file_sync.py
+++ PnPFileSync/src/pnp_file_sync.py
@@ -73,7 +73,9 @@ def process_namespace(apic, namespace):
         else:
             # need to look at checksum to see if need to update
             sha1 = get_sha1(rootDir+ '/' + filename)
-            print (filename, sha1)
+            print (filename, sha1, f.sha1)
+            if sha1 == f.sha1:
+                print("skip")
             result = f.update()
             print("Updated File:{file} ({id})".format(file=result.response.name, id=result.response.id))
 
